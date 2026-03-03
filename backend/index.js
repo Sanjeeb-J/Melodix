6100cce1763d93c2a95a780480a18e31aae63729
Sanjeeb J <sanjeebkodoth@gmail.com>
2026-03-03 09:09:53 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,7 @@ app.get("/", (req, res) => {
   res.send("Melodix API running");
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000 ;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
